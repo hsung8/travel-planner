75a2fcc734bacd9ace3784a29cf29ada760018be
hsung8 <hsung@atlantaregional.org>
2020-11-10 20:55:33 -0500

    dashboard styling

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { logoutUser } from "../../actions/authActions";
 import Logout from "../layout/Logout"
+import Search from "../Search/Search"
 
 class Dashboard extends Component {
 
@@ -12,20 +13,22 @@ class Dashboard extends Component {
     return (
       <div>
         <Logout />
-      
-      <div style={{ height: "75vh" }} className="container valign-wrapper">
-        <div className="row">
-          <div className="landing-copy col s12 center-align">
-            <h4>
-              <b>Hey there,</b> {user.name.split(" ")[0]}
-              <p className="flow-text grey-text text-darken-1">
-                You are logged into a full-stack{" "}
-                <span style={{ fontFamily: "monospace" }}>MERN</span> app 👏
+        <div style={{ height: "75vh" }} className="container valign-wrapper">
+          <div className="row">
+            <div className="col s12 center-align">
+              <h4>
+                <b>Hey there,</b> {user.name.split(" ")[0]}
+                <p className="flow-text grey-text text-darken-1">
+                  You are logged into a full-stack{" "}
+                  <span style={{ fontFamily: "monospace" }}>MERN</span> app 👏
               </p>
-            </h4>
+              </h4>
+            </div>
           </div>
-        </div>
-      </div>
+        </div >
+
+          <Search />
+
       </div>
     );
   }
