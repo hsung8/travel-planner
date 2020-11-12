0a08dfc1bf4f68d61071a78f7fe2a8988cc768b4
hsung8 <hsung@atlantaregional.org>
2020-11-12 18:53:54 -0500

    change buttons on the budget card

--- client/src/components/layout/Budget.js
+++ client/src/components/layout/Budget.js
@@ -9,11 +9,43 @@ class Budget extends Component {
                     <div style={{ width: "100%" }} className="card horizontal">
                         <div className="card-stacked">
                             <div className="card-content">
-                                <button>BUDGET</button><br />
-                                <button>HOTELS</button><br />
-                                <button>FLIGHTS</button><br />
-                                <button>RENTAL CARS</button><br />
-                                <button>ACTIVITIES</button><br />
+                            <Link
+                                to="/budget"
+                                className="btn btn-large hoverable purple accent-3"
+                                style={{
+                                    marginBottom: 10,
+                                    width: "200px"
+                                }}
+                            >BUDGET</Link><br />
+                                <Link
+                                to="/hotel"
+                                className="btn btn-large hoverable green accent-3"
+                                style={{
+                                    marginBottom: 10,
+                                    width: "200px"
+                                }}
+                            >HOTELS</Link><br />
+                            <Link to="/flights"
+                                className="btn btn-large hoverable blue accent-3"
+                                style={{
+                                    marginBottom: 10,
+                                    width: "200px"
+                                }}
+                            >FLIGHTS</Link><br />
+                            <Link to="/rental"
+                                className="btn btn-large hoverable black accent-3"
+                                style={{
+                                    marginBottom: 10,
+                                    width: "200px"
+                                }}
+                            >RENTAL CARS</Link><br />
+                            <Link to="/activity"
+                                className="btn btn-large hoverable orange accent-3"
+                                style={{
+                                    marginBottom: 10,
+                                    width: "200px"
+                                }}
+                            >ACTIVITIES</Link><br />
                             </div>
                         </div>
                     </div>
