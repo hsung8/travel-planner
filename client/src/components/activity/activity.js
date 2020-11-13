ac3daa9289a4ae6b8cfb4df9675ec677443faf93
Connor Cote <cotec4@gmail.com>
2020-11-12 20:25:07 -0500

    updating routes

--- client/src/components/activity/activity.js
+++ client/src/components/activity/activity.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Search from "../Search/Search.js";
 import SearchActivity from "../Search/SearchActivity.js";
 
 const Activity = () => {
