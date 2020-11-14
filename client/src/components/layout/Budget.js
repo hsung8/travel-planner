601142dd19d8aac9d6295b4021ed3ba0a5b0f474
Connor Cote <cotec4@gmail.com>
2020-11-14 12:44:50 -0500

    Updating flight for API and working through budget

--- client/src/components/layout/Budget.js
+++ client/src/components/layout/Budget.js
@@ -4,6 +4,26 @@ import Piechart from "../layout/Piechart";
 import Table from "../layout/Table"
 
 class Budget extends Component {
+    state = {
+        savingsGoal: "",
+        savingsLength: "",
+        savingsPerWeek: ""
+    };
+
+    handleSearchEvent = (e) => {
+        if (e.target.name === "savingsGoal") {
+            this.setState({ savingsGoal: e.target.value });
+        };
+        if (e.target.name === "savingsLength") {
+            this.setState({ savingsGoal: e.target.value });
+        };
+    };
+    
+    handleSubmit = (e) => {
+        console.log(this.state.savingsPerWeek);
+        this.setState({savingsPerWeek: parseFloat(this.state.savingsGoal) })
+    }
+
     render() {
         return (
             <div className="row">
@@ -58,16 +78,23 @@ class Budget extends Component {
                             <div className="card-content">
                                 <div className="col s4 center-align">
                                     <h6>Your goal: </h6>
-                                    <h4>2,000 USD </h4>
+                                    <form onSubmit={(res) => {
+                                        res.preventDefault();
+                                    }}>
+                                        <input onChange={this.handleSearchEvent} name="savingsGoal" className="savingsGoal" placeholder="What is your savings goal?" style={{ textAlign: "center" }}></input>
+                                        <input onChange={this.handleSearchEvent} name="savingsLength"className="savingsLength" placeholder="How many weeks to save?" style={{ textAlign: "center" }}></input>
+                                        <button onSubmit={this.handleSubmit} type="submit" className="btn btn-large waves-effect hoverable">Submit</button>
+                                    </form>
+                                    <h4 className="totalSavings"></h4>
                                     < br />
-
                                     <h6>Your estimates: </h6>
-                                    <h4>1,800 USD </h4>
+                                    <h4 className="totalSpend"></h4>
                                 </div>
                                 <div className="col s8 center-align">
                                     <Piechart />
                                 </div>
                             </div>
+                            <h5 className="" style={{ display: "" }}>You need to save {this.state.savingsPerWeek} each week to save {}! Get Saving!</h5>
                         </div>
                     </div>
                     <div style={{ width: "100%" }} className="card horizontal">
