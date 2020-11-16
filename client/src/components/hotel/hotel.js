c8a5570f1bf14fe511e7eca200a3981f425dd3bc
hsung8 <hsung@atlantaregional.org>
2020-11-15 19:49:13 -0500

    added dashboard button

--- client/src/components/hotel/hotel.js
+++ client/src/components/hotel/hotel.js
@@ -1,27 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import SearchHotel from "../Search/SearchHotel.js";
+import Logout2 from "../layout/Logout2"
 
 const Hotel = () => {
     return (
-      <div  className="container valign-wrapper">
-      <div className="row">
-          <div className="row">
-              <div className="col s12 center-align">
-                  <h4>
-                      BOOK IT AND GO, CATCH YOU OUTSIDE HOWBOUT THAT!
+        <div>
+            <Logout2 />
+            <div className="container valign-wrapper">
+                <div className="row">
+                    <div className="row">
+                        <div className="col s12 center-align">
+                            <h4>
+                                BOOK IT AND GO, CATCH YOU OUTSIDE HOWBOUT THAT!
               </h4>
-                  <p className="flow-text white-text text-lighten-1">
-                      A freaking cool tool to book your travel for your next trip !!!
+                            <p className="flow-text white-text text-lighten-1">
+                                A freaking cool tool to book your travel for your next trip !!!
               </p>
-                  <br />
-              </div>
-          </div>
-          <SearchHotel />
-      </div>
-  </div>
- 
-        
+                            <br />
+                        </div>
+                    </div>
+                    <SearchHotel />
+                </div>
+            </div>
+        </div>
+
+
     );
 };
 
